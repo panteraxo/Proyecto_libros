552e6f11e6eb5131388e62878d1038bcfcde1244
maki giuseppe de la vega silva <101688628+panteraxo@users.noreply.github.com>
2023-12-15 22:02:56 -0500

    first commit

---
+++ backend/config.js
@@ -0,0 +1,8 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+'mongodb+srv://root:root@proyecto-libros.qqp8fhf.mongodb.net/libros?retryWrites=true&w=majority'
+
+//Please create a free database for yourself
+//this database wil be deleted after tutorial
+
---
+++ frontend/src/pages/CreateBooks.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const CreateBooks = () => {
+  return (
+    <div>CreateBooks</div>
+  )
+}
+export default CreateBooks
